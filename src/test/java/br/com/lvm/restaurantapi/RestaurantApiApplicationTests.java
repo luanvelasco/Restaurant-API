719b36a258a2beb6ea99947d890865a0a996f00c
Luan Velasco <velascoluan@gmail.com>
2022-05-04 15:32:21 -0300

    First Commit - Basic Project

---
+++ src/test/java/br/com/lvm/restaurantapi/RestaurantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lvm.restaurantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
